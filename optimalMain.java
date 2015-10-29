d0c64104e9e0ea8c7be5b1245822ef903a9d1fdb
gac30 <gac30@pitt.edu>
2015-10-29 11:56:59 -0400

    Added comments

--- optimalMain.java
+++ optimalMain.java
@@ -4,28 +4,43 @@ import java.util.*;
 
 public class optimalMain {
 	public static void main(String [] args) throws IOException {
-		String choice;
+		String choice;	// Holds user selection
 		Scanner user_input = new Scanner(System.in);
 
 		String basePath = new File("").getAbsolutePath();
 
+		// Create lists of the indexes we already have in the DB
+		List<Long> threeVarIndexes = new ArrayList<>();
+		threeVarIndexes = getDBIndexes(3);
+		List<Long> fourVarIndexes = new ArrayList<>();
+		fourVarIndexes = getDBIndexes(4);
+		List<Long> fiveVarIndexes = new ArrayList<>();
+		fiveVarIndexes = getDBIndexes(5);
+
 		do {
+			// Ask user what they would like to do
 			System.out.println("");
 			System.out.println("2: Retrieve a network");
 			System.out.println("1: Optimize database");
 			System.out.println("0: Quit");
 			choice = user_input.next();
 
+			// Check if the user selected one of the possible options
+			// Prompt them to choose again if not one of the options
 			while(!choice.equals("0") && !choice.equals("1") && !choice.equals("2")) {
 				System.out.println("Invalid choice. Please select either 0, 1, or 2.");
 				choice = user_input.next();
 			}
 
+			// Retrieve network from Database, and mutate that network 50 times
 			if(choice.equals("2")) {
 				String u;
+				// Ask user for number of variables
 				System.out.println("Number of variables (3-5):");
 				u = user_input.next();
 
+				// Check if user selected a value between 3 and 5
+				// Prompt user to choose again if invalid entry
 				while(!u.equals("3") && !u.equals("4") && !u.equals("5")) {
 					System.out.println("Invalid choice. Please select either 3, 4, or 5.");
 					u = user_input.next();
@@ -33,11 +48,14 @@ public class optimalMain {
 
 				int numVars = Integer.parseInt(u);
 
+				// Ask user from truth table file
 				System.out.println("Truth table file:");
 				String truthTableFilename = user_input.next();
 
+				// Read in truth table file
 				String[] lines = readUserTableFile(numVars,truthTableFilename);
 
+				// Check to see if numVars matches up with the number of number of rows in truth table
 				if(lines.length != ((int) (Math.pow(2,numVars)))) {
 					System.out.println("Error with user input:\nNumber of variables do not match with the number of truth table values");
 				} else {
@@ -57,6 +75,8 @@ public class optimalMain {
 
 					int sopCost = calcSOPCost(numVars,table);
 
+					// User truth table doesn't exist in database
+					// Create SOP
 					if(cost == -1) {
 						System.out.println("File does not exist. Creating SOP.\n");
 
@@ -66,7 +86,7 @@ public class optimalMain {
 						network = new BooleanTree(numVars, table, sumOfProducts, sopTerms);
 
 						sopCreated = true;
-					} else if (cost>sopCost) {
+					} else if (cost>sopCost) {	// Cost in DB is greater than SOP, Create SOP
 						System.out.println("Database network cost greater than SOP cost. Creating SOP\n");
 
 						sumOfProducts = getSumOfProducts(numVars,table);
@@ -75,7 +95,7 @@ public class optimalMain {
 						network = new BooleanTree(numVars, table, sumOfProducts, sopTerms);
 
 						sopCreated = true;
-					} else {
+					} else {	// Truth table exists in DB, read it in
 						ArrayList<String> linesFromFile = readDatabaseFile(indexFilename);
 
 						linesFromFile.remove(0);
@@ -83,59 +103,107 @@ public class optimalMain {
 						network = new BooleanTree(numVars, table, linesFromFile);
 					}
 
-					// Output Network
+					// Output Network to user
 					String s = network.printNetwork();
 					System.out.println(s);
 					cost = network.getCost();
 					System.out.println(cost);
 
+					// Write file to db
 					boolean success = writeDBFile(network,indexFilename);
-
 					if(success) {
 						System.out.println("Network written to database");
+
+						// Add the truth table to list of things in db
+						if(numVars==3) {
+							if(!threeVarIndexes.contains(index)) {
+								threeVarIndexes.add(index);
+							}
+						} else if(numVars == 4) {
+							if(!fourVarIndexes.contains(index)) {
+								fourVarIndexes.add(index);
+							}
+						} else {
+							if(!fiveVarIndexes.contains(index)) {
+								fiveVarIndexes.add(index);
+							}
+						}
 					}
 
-
+					// Mutate the user entered truth table network 50 times
 					for(int i=0;i<50;i++) {
 						network.mutate();
 
 						// Output Network
-						s = network.printNetwork();
-						System.out.println(s);
+						//s = network.printNetwork();
+						//System.out.println(s);
 						cost = network.getCost();
-						System.out.println(cost);
+						//System.out.println(cost);
 
 						sopCost = calcSOPCost(numVars, network.getTruthTable());
 
+						// Check if the cost is less than SOP
+						// Only save to DB if less than SOP
 						if(network.getCost() < sopCost) {
 							index = calcIndex(network.getTruthTable());
 							indexFilename = basePath+"\\"+numVars+"var\\"+index+".txt";
 
 							cost = getIndexCost(indexFilename);
 
+							// See if the cost is less than the cost currently in db
 							if(network.getCost() < cost || cost == -1) {
+								// Write to DB
 								success = writeDBFile(network, indexFilename);
 								if(success) {
 									System.out.println("Network written to database");
+
+									// Add the truth table to list of things in db
+									if(numVars==3) {
+										if(!threeVarIndexes.contains(index)) {
+											threeVarIndexes.add(index);
+										}
+									} else if(numVars == 4) {
+										if(!fourVarIndexes.contains(index)) {
+											fourVarIndexes.add(index);
+										}
+									} else {
+										if(!fiveVarIndexes.contains(index)) {
+											fiveVarIndexes.add(index);
+										}
+									}
 								}
 							}
 						}
 					}
 				}
-			} else if(choice.equals("1")) {
-				for(int i=0;i<5;i++) {
-					String u;
-					System.out.println("Number of variables (3-5):");
+			} else if(choice.equals("1")) {	// Optimize DB
+				String u;
+				// Prompt user to select DB folder to optimize
+				System.out.println("Number of variables (3-5):");
+				u = user_input.next();
+
+				// Check if user selected a value between 3 and 5
+				// Prompt user to choose again if invalid entry
+				while(!u.equals("3") && !u.equals("4") && !u.equals("5")) {
+					System.out.println("Invalid choice. Please select either 3, 4, or 5.");
 					u = user_input.next();
+				}
+				int numVars = Integer.parseInt(u);
 
-					while(!u.equals("3") && !u.equals("4") && !u.equals("5")) {
-						System.out.println("Invalid choice. Please select either 3, 4, or 5.");
-						u = user_input.next();
+				// Choose 5 different networks to mutate 100 times each
+				for(int i=0;i<5;i++) {
+					// Select a network to mutate
+					long index;
+					if(numVars==3) {
+						int indexListVal = chooseIndex(threeVarIndexes.size());
+						index = threeVarIndexes.get(indexListVal);
+					} else if(numVars==4) {
+						int indexListVal = chooseIndex(fourVarIndexes.size());
+						index = fourVarIndexes.get(indexListVal);
+					} else {
+						int indexListVal = chooseIndex(fiveVarIndexes.size());
+						index = fiveVarIndexes.get(indexListVal);
 					}
-
-					int numVars = Integer.parseInt(u);
-
-					long index = chooseIndex(numVars);
 					int[] table = getTableFromIndex(numVars,index);
 
 					String indexFilename = basePath+"\\"+numVars+"var\\"+index+".txt";
@@ -153,11 +221,14 @@ public class optimalMain {
 						break;
 					}
 
+					// Read in DB file
+					// Remove cost from file inputs
+					// Create network
 					ArrayList<String> linesFromFile = readDatabaseFile(indexFilename);
 					linesFromFile.remove(0);
-
 					BooleanTree network = new BooleanTree(numVars,table,linesFromFile);
 
+					// Mutate network 100 times
 					for(int j=0;j<100;j++) {
 						network.mutate();
 
@@ -178,6 +249,20 @@ public class optimalMain {
 								boolean success = writeDBFile(network, indexFilename);
 								if(success) {
 									System.out.println("Network written to database");
+
+									if(numVars==3) {
+										if(!threeVarIndexes.contains(index)) {
+											threeVarIndexes.add(index);
+										}
+									} else if(numVars == 4) {
+										if(!fourVarIndexes.contains(index)) {
+											fourVarIndexes.add(index);
+										}
+									} else {
+										if(!fiveVarIndexes.contains(index)) {
+											fiveVarIndexes.add(index);
+										}
+									}
 								}
 							}
 						}
@@ -186,9 +271,22 @@ public class optimalMain {
 			}
 
 		} while(!choice.equals("0"));
+
+		// Save index lists
+		saveDBIndexes(3, threeVarIndexes);
+		saveDBIndexes(4, fourVarIndexes);
+		saveDBIndexes(5, fiveVarIndexes);
 	}
 
 
+	/********************
+	Read in user's truth table file.
+
+	int numVars: number of variables corresponding to the truth table
+	String filename: filepath of the truth table file entered by user
+
+	Return: a String array of all the lines from the file
+	********************/
 	public static String[] readUserTableFile(int numVars, String filename) {
 		ArrayList<String> lines = new ArrayList<String>();
 		int i=0;
@@ -208,7 +306,16 @@ public class optimalMain {
 		return lines.toArray(linesArr);
 	}
 
-	public static int[] getTableFromFile(int numVars,String[] lines) {
+	/********************
+	Using the string array retrieved from readUserTableFile(),
+	convert to an integer array which represents the truth table
+
+	int numVars: number of varaibles corresponding to the truth table
+	String[] lines: string array returned from readUserTableFile()
+
+	Return: an integer array which corresponds to the truth table
+	********************/
+	public static int[] getTableFromFile(int numVars, String[] lines) {
 		int[] table = new int[(int) (Math.pow(2,numVars))];
 		for(int i=0;i<lines.length;i++) {
 			String line = lines[i];
@@ -224,6 +331,14 @@ public class optimalMain {
 		return table;
 	}
 
+	/********************
+	Calculate the DB index value of a truth table
+	Basically a boolean to decimal conversion
+
+	int[] table: integer array corresponding to a truth table
+
+	Return: a long value which can be used to reference DB files
+	********************/
 	public static long calcIndex(int[] table) {
 		long index = 0;
 		for(int i=0;i<table.length;i++) {
@@ -233,6 +348,14 @@ public class optimalMain {
 		return index;
 	}
 
+	/********************
+	Find the cost of a network from it's DB file
+
+	String indexFilename: filepath of the db file to get the cost of
+
+	Return: integer value of cost retrieved from DB file
+		will return -1 if DB file does not exist, or errors in any other way
+	********************/
 	public static int getIndexCost(String indexFilename) {
 		try {
 			BufferedReader buff = new BufferedReader(new FileReader(indexFilename));
@@ -248,6 +371,14 @@ public class optimalMain {
 		}
 	} 
 
+	/********************
+	Create Sum-Of-Products array
+
+	int numVars: number of varaibles corresponding to the truth table
+	int[] table: integer array representing a truth table
+
+	Return: multidimensional integer array which contains 0's and 1's representing each SOP term
+	********************/
 	public static int[][] getSumOfProducts(int numVars, int[] table) {
 		int[][] sumOfProducts = new int[(int) (Math.pow(2,numVars))][numVars];
 		int sopTerms=0;
@@ -277,6 +408,13 @@ public class optimalMain {
 		return sumOfProducts;
 	}
 
+	/********************
+	Calculate the number of Sum-Of-Product terms
+
+	int[] table: integer array representing a truth table
+
+	Return: integer value which is the number of SOP terms
+	********************/
 	public static int getSOPTerms(int[] table) {
 		int sopTerms =0;
 		for(int i=0;i<table.length;i++) {
@@ -287,6 +425,13 @@ public class optimalMain {
 		return sopTerms;
 	}
 
+	/********************
+	Read the lines of a DB file into an arraylist
+
+	String indexFilename: filepath of a db file
+
+	Return: arraylist containing all the lines of a DB file
+	********************/
 	public static ArrayList<String> readDatabaseFile(String indexFilename) {
 		ArrayList<String> lines = new ArrayList<String>();
 		String line;
@@ -304,6 +449,14 @@ public class optimalMain {
 		return lines;
 	}
 
+	/********************
+	Calculate the cost of a SOP representation of a truth table
+
+	int numVars: number of variables corresponding to the truth table
+	int[] table: integer array representing a truth table
+
+	Return: integer value representing the cost of the sum of products 
+	********************/
 	public static int calcSOPCost(int numVars, int[] table) {
 		int cost = 0;
 		int andGates = 0;
@@ -319,21 +472,30 @@ public class optimalMain {
 		return cost;
 	}
 
-	public static int chooseNumVar() {
+	/********************
+	Generate a random value between 0 and possible
+
+	int possible: number of possible values to choose from
+
+	Return: random integer between 0 and possible
+	********************/
+	public static int chooseIndex(int possible) {
 		Random randGen = new Random(System.currentTimeMillis());
-		int numVar = randGen.nextInt(3) + 3;
-
-		return numVar;
-	}
-
-	public static int chooseIndex(int numVars) {
-
-		Random randGen = new Random(System.currentTimeMillis());
-		int index = randGen.nextInt((int) (Math.pow(2,(Math.pow(2,numVars)))));
+		int index = randGen.nextInt(possible);
 
 		return index;
 	}
 
+	/********************
+	Write file to DB
+
+	BooleanTree network: booleantree representation of network to output
+	String indexFilename: filepath of the location to output the network
+
+	Return: boolean value of success
+		true: network has been written to db
+		false: error with writing network
+	********************/
 	public static boolean writeDBFile(BooleanTree network, String indexFilename) {
 		try {
 			FileWriter writer = new FileWriter(new File(indexFilename));
@@ -350,7 +512,14 @@ public class optimalMain {
 
 	}
 
+	/********************
+	Generate a truth table based on a db index value
 
+	int numVars: number of input variables corresponding to the truth table
+	long index: decimal value representing the truth table
+
+	Return: integer array representing a truth table
+	********************/
 	public static int[] getTableFromIndex(int numVars, long index) {
 		int[] table = new int[(int) (Math.pow(2,numVars))];
 		String s = Long.toBinaryString(index);
@@ -359,6 +528,72 @@ public class optimalMain {
 		}
 
 		return table;
+	}
+
+	/********************
+	Create a list of all the index values in the DB
+
+	int numVars: number of input variables corresponding to the DB
+
+	Return: list of longs containing all index values in the DB
+	********************/
+	public static List<Long> getDBIndexes(int numVars) {
+		List<Long> indexes = new ArrayList<>();
+
+		String basepath = new File("").getAbsolutePath();
+		String dbFilePath = basepath+"\\"+numVars+"var\\indexes.txt";
+
+		File dbFile = new File(dbFilePath);
+
+		if(dbFile.exists() == true) {
+			try {
+				BufferedReader buff = new BufferedReader(new FileReader(dbFilePath));
+				String line;
+
+				while((line=buff.readLine())!= null) {
+					long hold = Long.valueOf(line).longValue();
+					indexes.add(hold);
+				}
+			} catch (IOException e) {
+				System.out.println("Error reading DB index file");
+			}
+		} else {
+			for(long i=0;i<(long) Math.pow(2,Math.pow(2,numVars));i++) {
+				String indexFilename = basepath+"\\"+numVars+"var\\"+i+".txt";
+				int hold = getIndexCost(indexFilename);
+
+				//System.out.println(i);
+
+				if(hold > 0) {
+					indexes.add(i);
+				}
+			}
+		}
+
+		return indexes;
+	}
+
+	/********************
+	Save the list of indexes to a file in the db
+
+	int numVars: number of input variables corresponding to the truth table
+	List<Long> indexes: list of indexes in db
+	********************/
+	public static void saveDBIndexes(int numVars, List<Long> indexes) {
+		String basepath = new File("").getAbsolutePath();
+		String dbFilePath = basepath+"\\"+numVars+"var\\indexes.txt";
+
+		try {
+			FileWriter writer = new FileWriter(new File(dbFilePath));
+
+			for(int i=0;i<indexes.size();i++) {
+				writer.write(Long.toString(indexes.get(i))+"\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("Error saving DB index file");
+		}
 	}
 
 }
